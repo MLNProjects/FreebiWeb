35bafb8807e31213e94f0a5e02af2a1ad81202a7
blondieboi <alexandre.blondot@gmail.com>
2019-11-09 20:57:13 +0100

    header boilderplate

--- src/MapView.jsx
+++ src/MapView.jsx
@@ -8,7 +8,12 @@ const position = [59.35, 18.06];
 
 const MapView = ({ locations }) => {
   return (
-    <Map center={position} zoom={13} style={{ width: "100%", height: "100%" }}>
+    <Map
+      zoomControl={false}
+      center={position}
+      zoom={13}
+      style={{ width: "100%", height: "100%" }}
+    >
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
