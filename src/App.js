83de534c028a8bb66bcef0fdebf99f45b3b5e39f
blondieboi <alexandre.blondot@gmail.com>
2019-11-09 19:57:00 +0100

    created header component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import MapView from './MapView';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import MapView from "./MapView";
+import Header from "./Components/Header/header";
 
 function App() {
-	const [isOn, setIsOn] = React.useState(false);
-	const [positions, setPositions] = React.useState([]);
+  const [isOn, setIsOn] = React.useState(false);
+  const [positions, setPositions] = React.useState([]);
 
-	navigator.geolocation.getCurrentPosition(p => setPositions([...positions, p]));
+  navigator.geolocation.getCurrentPosition(p =>
+    setPositions([...positions, p])
+  );
 
-	const updateLocations = () => {
-		if ('geolocation' in navigator) {
-			console.log(positions);
-			navigator.geolocation.getCurrentPosition(p => setPositions([...positions, p]));
-		} else {
-			/* geolocation IS NOT available */
-		}
-	};
+  const updateLocations = () => {
+    if ("geolocation" in navigator) {
+      console.log(positions);
+      navigator.geolocation.getCurrentPosition(p =>
+        setPositions([...positions, p])
+      );
+    } else {
+      /* geolocation IS NOT available */
+    }
+  };
 
-	React.useEffect(() => {
-		const interval = setInterval(() => {
-			updateLocations();
-		}, 1000);
-		return () => clearInterval(interval);
-	}, []);
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      updateLocations();
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
 
-	return isOn ? (
-		<div style={{ height: '100vh', width: '100vw' }}>
-			<MapView locations={positions}></MapView>
-		</div>
-	) : (
-		<button onClick={() => setIsOn(true)}>turn on</button>
-	);
+  return isOn ? (
+    <div style={{ height: "100vh", width: "100vw" }}>
+      <Header />
+      <MapView locations={positions}></MapView>
+    </div>
+  ) : (
+    <button onClick={() => setIsOn(true)}>turn on</button>
+  );
 }
 
 export default App;
---
+++ src/Components/Header/header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./header.css";
+
+const Header = () => {
+  return (
+    <div className="header-wrapper">
+      <nav>menu</nav>
+      <h1 className="company-title">Freebee</h1>
+    </div>
+  );
+};
+
+export default Header;
