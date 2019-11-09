fe85a98d7ffae4b9b8c8d8ac464b1e6ef1c95164
Erland <nicolasjonason@gmail.com>
2019-11-09 22:50:50 +0100

    fix marker position

--- src/MapView.jsx
+++ src/MapView.jsx
@@ -5,9 +5,12 @@ import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 import icon from 'leaflet/dist/images/marker-icon.png';
 import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 
+let iconImg = new Image(icon);
+
 let DefaultIcon = L.icon({
 	iconUrl: icon,
 	shadowUrl: iconShadow,
+	iconAnchor: [12, 41],
 });
 
 L.Marker.prototype.options.icon = DefaultIcon;
