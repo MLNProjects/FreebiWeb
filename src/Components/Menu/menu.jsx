b4917fc0a39929f73aef99c755f23ff9f04acd6d
blondieboi <alexandre.blondot@gmail.com>
2019-11-10 17:33:57 +0100

    added menu autoclose on click

--- src/Components/Menu/menu.jsx
+++ src/Components/Menu/menu.jsx
@@ -4,13 +4,24 @@ import { useStateValue } from "../../utilities/StateManagement/stateManagement";
 import { BrowserRouter as Router, Link } from "react-router-dom";
 
 const Menu = () => {
-  const [{ menu }] = useStateValue();
+  const [{ menu }, dispatch] = useStateValue();
+
+  const closeMenu = () => {
+    dispatch({
+      type: "toggleMenu",
+      menu: { toggle: false }
+    });
+  };
 
   return (
     <div className={menu.toggle ? "overlay active-nav" : "overlay"}>
       <div className="overlay-content">
-        <Link to="/">Home</Link>
-        <Link to="/signup">Sign Up</Link>
+        <Link onClick={closeMenu} to="/">
+          Home
+        </Link>
+        <Link onClick={closeMenu} to="/signup">
+          Sign Up
+        </Link>
       </div>
     </div>
   );
