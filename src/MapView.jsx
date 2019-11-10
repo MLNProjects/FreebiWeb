e9da4fb25ef26595e93b0881846338fe7208f40f
blondieboi <alexandre.blondot@gmail.com>
2019-11-10 10:11:16 +0100

    moved zoom control to bottom right

--- src/MapView.jsx
+++ src/MapView.jsx
@@ -1,16 +1,16 @@
-import 'leaflet/dist/leaflet.css';
-import React from 'react';
-import L from 'leaflet';
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
-import icon from 'leaflet/dist/images/marker-icon.png';
-import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+import "leaflet/dist/leaflet.css";
+import React from "react";
+import L from "leaflet";
+import { Map, Marker, Popup, TileLayer, ZoomControl } from "react-leaflet";
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
 
 let iconImg = new Image(icon);
 
 let DefaultIcon = L.icon({
-	iconUrl: icon,
-	shadowUrl: iconShadow,
-	iconAnchor: [12, 41],
+  iconUrl: icon,
+  shadowUrl: iconShadow,
+  iconAnchor: [12, 41]
 });
 
 L.Marker.prototype.options.icon = DefaultIcon;
@@ -18,25 +18,31 @@ L.Marker.prototype.options.icon = DefaultIcon;
 const position = [59.35, 18.06];
 
 const MapView = ({ locations }) => {
-	return (
-		<Map center={position} zoom={13} style={{ width: '100%', height: '100%' }}>
-			<TileLayer
-				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-				attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-			/>
-			{locations.map((l, i) => {
-				return (
-					<Marker position={l} key={i}>
-						<Popup>
-							A pretty CSS3 popup.
-							<br />
-							Easily customizable.
-						</Popup>
-					</Marker>
-				);
-			})}
-		</Map>
-	);
+  return (
+    <Map
+      zoomControl={false}
+      center={position}
+      zoom={13}
+      style={{ width: "100%", height: "100%" }}
+    >
+      <ZoomControl position="bottomright" />
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+      {locations.map((l, i) => {
+        return (
+          <Marker position={l} key={i}>
+            <Popup>
+              A pretty CSS3 popup.
+              <br />
+              Easily customizable.
+            </Popup>
+          </Marker>
+        );
+      })}
+    </Map>
+  );
 };
 
 export default MapView;
