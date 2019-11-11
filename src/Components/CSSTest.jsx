9a4e4df3741d5975963117be6670a99793a4d65b
Erland <nicolasjonason@gmail.com>
2019-11-11 22:09:05 +0100

    add css

--- src/Components/CSSTest.jsx
+++ src/Components/CSSTest.jsx
@@ -6,9 +6,13 @@ const CSSTest = ({}) => {
       <h1>This is a h1</h1>
       <h2>This is a h2</h2>
       <h3>This is a h3</h3>
-      <button>PRIMARY</button>
-      <button>SECONDARY</button>
-      <button>WARNING</button>
+      <p>This is a paragraph</p>
+      <p>
+        This word is <em>emphasized</em>
+      </p>
+      <button className="primary">PRIMARY</button>
+      <button className="secondary">SECONDARY</button>
+      <button className="warning">WARNING</button>
     </div>
   );
 };
