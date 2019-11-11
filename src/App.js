492419ffe93519b09c152cff2045d6e956c298fe
Erland <nicolasjonason@gmail.com>
2019-11-11 21:10:30 +0100

    remove header from app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
 
-//Components
-import Header from './Components/Header/Header';
-
 //Routes
 import Map from './Routes/Map/Map';
 import SignUp from './Routes/SignUp/SignUp';
@@ -22,8 +19,8 @@ function App() {
 			<Router>
 				<Switch>
 					<React.Fragment>
-						{/* <Header /> */}
 						<div>
+							{/* <Route exact path="/csstest" component={CSSTest}></Route> */}
 							<Route exact path="/" component={Map} />
 							<Route exact path="/signup" component={SignUp} />
 							<Route exact path="/login" component={Login} />
